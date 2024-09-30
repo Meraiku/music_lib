a85a47b0606e2ca5cce6f8dce731aad2e88e4071
Meraiku <meraikuu@gmail.com>
2024-09-30 19:33:49 +0300

    goose migrations

--- sql/migrations/001_songs.sql
+++ sql/migrations/001_songs.sql
@@ -1,10 +1,16 @@
+-- goose +Up 
+
 CREATE TABLE IF NOT EXISTS songs(
     id UUID PRIMARY KEY,
-    band TEXT NOT NULL,
-    song TEXT NOT NULL,
+    band VARVHAR(70) NOT NULL,
+    song VARVHAR(255) NOT NULL,
     release_date TIMESTAMP NOT NULL,
     lirics TEXT NOT NULL,
-    link TEXT NOT NULL,
+    link VARVHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
+
+-- goose +Down
+
+DROP TABLE IF EXISTS songs;
